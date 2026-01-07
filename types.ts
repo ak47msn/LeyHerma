0d554698a73bcd7d346fce9cdfe6a91fbb9a3e6f
Your Name <you@example.com>
2026-01-07 14:57:34 +0100

    Init

---
+++ types.ts
@@ -0,0 +1,17 @@
+
+export interface NavItem {
+  label: string;
+  href: string;
+}
+
+export interface Testimonial {
+  name: string;
+  text: string;
+  date: string;
+}
+
+export interface GalleryImage {
+  url: string;
+  alt: string;
+  caption?: string;
+}
